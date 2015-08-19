e267767d8fb309e9b3b74022e220598e8f39d5ae
absyah <ardian.bahtiarsyah@gmail.com>
2015-08-19 20:51:07 +0700

    added mongo model

--- todo-app.js
+++ todo-app.js
@@ -1,11 +1,11 @@
+Task = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
 
   Template.body.helpers({
-    tasks: [
-      {text: "This is task 1"},
-      {text: "This is task 2"},
-      {text: "This is task 3"},
-    ]
+    tasks: function () {
+      return Task.find({});
+    }
   });
 
 }
