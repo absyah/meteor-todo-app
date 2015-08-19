37d0daa52f083cdb180bb2f8e19580e69d30db0b
absyah <ardian.bahtiarsyah@gmail.com>
2015-08-19 21:00:57 +0700

    added task creation process

--- todo-app.js
+++ todo-app.js
@@ -4,7 +4,26 @@ if (Meteor.isClient) {
 
   Template.body.helpers({
     tasks: function () {
-      return Task.find({});
+      return Task.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+
+      // Get value from form element
+      var text = event.target.text.value;
+
+      // Insert to Mongo
+      Task.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      // Clear form
+      event.target.text.value = "";
     }
   });
 
